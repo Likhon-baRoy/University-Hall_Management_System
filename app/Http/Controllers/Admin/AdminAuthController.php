1f71ca9568473030115a71c9f08b3d141cfefc1d
Likhon Barai <likhonhere007@gmail.com>
2024-12-30 21:00:51 +0600

    added email/username/cell in login input filed

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -23,10 +23,10 @@ class AdminAuthController extends Controller
     ]);
 
     // Login attempt
-    if ( Auth::guard('admin') -> attempt([
-      'email' => $request -> auth,
-      'password' => $request -> password
-    ])) {
+    if ( Auth::guard('admin') -> attempt(['username' => $request -> auth, 'password' => $request -> password]) ||
+         Auth::guard('admin') -> attempt(['email' => $request -> auth, 'password' => $request -> password]) ||
+         Auth::guard('admin') -> attempt(['cell' => $request -> auth, 'password' => $request -> password])
+    ) {
       return redirect() -> route('admin.dashboard');
     } else {
       return redirect() -> route('admin.login.page') -> with('warning', 'Email/Password not matched');
