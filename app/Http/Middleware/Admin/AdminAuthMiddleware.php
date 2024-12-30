cfca63a4eaf5ac5d804955982ba9221d7bf9bf03
Likhon Barai <likhonhere007@gmail.com>
2024-12-30 21:48:08 +0600

    check if user is logged-in to access admin-panel

--- app/Http/Middleware/Admin/AdminAuthMiddleware.php
+++ app/Http/Middleware/Admin/AdminAuthMiddleware.php
@@ -5,16 +5,21 @@ namespace App\Http\Middleware\Admin;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuthMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+   */
+  public function handle(Request $request, Closure $next): Response {
+
+    if (Auth::guard('admin') -> check()) {
+      return $next($request);
     }
+
+    return redirect() -> route('admin.login.page');
+  }
 }
