678c928cd2376fef05bd6dc907952504aa8ef42e
Likhon Barai <likhonhere007@gmail.com>
2025-01-01 08:48:21 +0600

    1-m relationship with Role, 1 role can have many u

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model {
   // give permission to all column available in the Role table
   protected $guarded = [];
+
+  // get role users
+  public function users() {
+    return $this -> hasMany(Admin::class, 'role_id', 'id');
+  }
 }
