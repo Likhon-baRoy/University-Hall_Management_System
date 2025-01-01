813a728e731c27b7b306d9d94a22c194da92f175
Likhon Barai <likhonhere007@gmail.com>
2025-01-01 15:22:58 +0600

    send notification to notify on account activation

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends User
 {
+  use Notifiable;
+
   protected $guarded = []; // empty array mean all column is accessable
 
   // get user role
