a0a58df1c08fb672b360c702ea9ecf732acd8083
Likhon Barai <likhonhere007@gmail.com>
2025-01-08 10:22:10 +0600

    modified db:seeder to add default hall entry

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,34 +2,67 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Hall;
 use App\Models\Admin;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-
 class DatabaseSeeder extends Seeder
 {
-  /**
-   * Seed the application's database.
-   */
   public function run(): void
   {
-    // User::factory(10)->create();
-    /*
-     *       User::factory()->create([
-     *         'name' => 'Test User',
-     *         'email' => 'test@example.com',
-     *       ]);
-     *
-     *  */
+    // Create Permissions
+    $permissions = ['Slider', 'Our Team', 'Testimonials', 'Admins', 'Settings', 'Posts', 'Portfolio', 'Clients'];
+
+    foreach ($permissions as $permission) {
+      Permission::create([
+        'name' => $permission,
+        'slug' => \Str::slug($permission),
+      ]);
+    }
+
+    // Create Super Admin Role
+    $role = Role::create([
+      'name' => 'Super Admin',
+      'slug' => 'super-admin',
+      'permissions' => json_encode($permissions), // Assign all permissions directly
+    ]);
+
+    // Create Super Admin User
     Admin::create([
-      'name'        => 'Provider',
-      'email'       => 'provider@gmail.com',
-      'cell'        => '01532389132',
-      'username'    => 'provider',
-      'password'    => Hash::make('123')
+      'name' => 'Provider',
+      'email' => 'provider@gmail.com',
+      'cell' => '01532389132',
+      'username' => 'provider',
+      'password' => Hash::make('123'),
+      'role_id' => $role->id,
+    ]);
+
+    // Create Existing Halls
+    Hall::create([
+      'name' => 'Mokbul Hossain Hall',
+      'gender' => 'male',
+      'location' => 'City Campus, Savar',
+    ]);
+
+    Hall::create([
+      'name' => 'Fatema Hall',
+      'gender' => 'female',
+      'location' => 'City Campus, Savar',
+    ]);
+
+    Hall::create([
+      'name' => 'Mona Hall',
+      'gender' => 'female',
+      'location' => 'Khagan Bazar, Birulia, Savar',
+    ]);
+
+    Hall::create([
+      'name' => 'Fazlur Rahaman Hall',
+      'gender' => 'male',
+      'location' => 'Khagan Bazar, Birulia, Savar',
     ]);
   }
 }
