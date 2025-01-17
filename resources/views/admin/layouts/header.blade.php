f4043c22a0862242d2128582820c0c9e9db29320
Likhon Barai <likhonhere007@gmail.com>
2025-01-17 20:51:23 +0600

    logo image set inside url()

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -4,10 +4,10 @@
   <!-- Logo -->
   <div class="header-left">
     <a href="index.html" class="logo">
-      <img src="admin/assets/img/logo.png" alt="Logo">
+      <img src="{{ url('admin/assets/img/logo.png') }}" alt="Logo">
     </a>
     <a href="index.html" class="logo logo-small">
-      <img src="admin/assets/img/logo-small.png" alt="Logo" width="30" height="30">
+      <img src="{{ url('admin/assets/img/logo-small.png') }}" alt="Logo" width="30" height="30">
     </a>
   </div>
   <!-- /Logo -->
